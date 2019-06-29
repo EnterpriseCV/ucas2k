cd6785f4e4cf8fcab66019656d711cd53932f2d6
EnterpriseCV <502101600@qq.com>
2019-06-29 11:09:18 +0800

    workType

--- src/main/java/nju/ucas2k/controller/FeeController.java
+++ src/main/java/nju/ucas2k/controller/FeeController.java
@@ -1,5 +1,8 @@
 package nju.ucas2k.controller;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiParam;
 import nju.ucas2k.model.Fee;
 import nju.ucas2k.service.FeeService;
 import nju.ucas2k.util.PageableList;
@@ -23,9 +26,27 @@ public class FeeController {
         return new ResEntity(200,feeService.addFee(fee),null);
     }
 
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "pageNum", required = true, value = "需查询的页码，为-1时查询全部"),
+            @ApiImplicitParam(name = "pageSize", required = true, value = "每页包含的记录数，pageNum为正整数时有效"),
+            @ApiImplicitParam(name = "articleId", required = true, value = "费用所属文章的id，不启用时设置为-1"),
+            @ApiImplicitParam(name = "workerType", value = "人员类型，启用时为inner或outer"),
+            @ApiImplicitParam(name = "workerId", required = true, value = "人员id，启用workerType且值为正整数时有用，不启用设为-1"),
+            @ApiImplicitParam(name = "workType", value = "工作类型"),
+            @ApiImplicitParam(name = "startTime", value = "开始时间"),
+            @ApiImplicitParam(name = "endTime", value = "结束时间")
+    })
     @GetMapping("/fee")
-    public ResEntity getFee(int pageNum, int pageSize, long articleId, String workerType, long workerId, Date startTime, Date endTime){
-        return new ResEntity(200,feeService.selectByConditions(pageNum,pageSize,articleId,workerType,workerId,startTime,endTime),null);
+    public ResEntity<PageableList<Fee>> getFee(int pageNum,
+                                               int pageSize,
+                                               long articleId,
+                                               String workerType,
+                                               long workerId,
+                                               String workType,
+                                               Date startTime,
+                                               Date endTime){
+        return new ResEntity(200,feeService.selectByConditions(pageNum,pageSize,articleId,workerType,workerId,workType,startTime,endTime),null);
     }
 
     @PutMapping("/fee")
--- src/main/java/nju/ucas2k/service/FeeService.java
+++ src/main/java/nju/ucas2k/service/FeeService.java
@@ -11,6 +11,6 @@ public interface FeeService {
     public int deleteById(long id);
     public int deleteByArticleId(long articleId);
     public int deleteByWorker(String workerType,long workerId);
-    public PageableList<Fee> selectByConditions(int pageNum,int pageSize,long articleId, String workerType, long workerId, Date startTime,Date endTime);//都可选，不用文章id传-1，不用workerType传null，workerId必须和workerType一起用，单独用workerType的话workerId传-1
+    public PageableList<Fee> selectByConditions(int pageNum,int pageSize,long articleId, String workerType, long workerId,String workType, Date startTime,Date endTime);//都可选，不用文章id传-1，不用workerType传null，workerId必须和workerType一起用，单独用workerType的话workerId传-1
     public int updateFee(Fee fee);
 }
